86362d3ffd942332108a390ee5c38f86b2ba758b
Mariah <mariahschock@gmail.com>
2022-05-02 17:12:18 -0700

    fixed button in html

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
-
+const form = document.getElementById('add-ingredient');
 // let state
+let recipes = [];
 
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const addRecipe = new FormData(form);
+
+    const recipe = {
+        ingredient: addRecipe.get('ingredient'),
+        quantity: addRecipe.get('quantity'),
+        measurement: addRecipe.get('measurement'),
+    };
+    recipes.push(recipe);
+
+    recipe = [];
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
